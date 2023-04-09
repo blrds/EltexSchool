0de9e9dae1a51687c7827612e6bdaa4149ebb3c4
blrds <blad.mob@mail.ru>
2023-04-09 16:41:41 +0700

    1.10

--- 3/1/10/10.c
+++ 3/1/10/10.c
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]){
     char h[]="ls| head -3 | tail -1";
     char *path="/bin/";
     int i=0,length=0,j;
-    char** tokens=strSplit(h,"|",&length);
+    char** tokens=strSplit(argv[1],"|",&length);
     char* token, *arg;
     char str[256]={0};
     for(i=0;i<length;i++){
@@ -33,14 +33,13 @@ int main(int argc, char* argv[]){
         if ( fork()== 0 ){
             char** args=strSplit(tokens[i]," ",&j);
             char tmp[256]={0};
-            fprintf(stderr,"child: iteration %d\n",i);
             strcpy(tmp,args[0]);
             sprintf(args[0],"%s%s",path,tmp);
             if(args[1]!=NULL){
-                args=(char**)realloc(args,sizeof(char*)*3);
+                args=(char**)realloc(args,sizeof(char*)*4);
                 args[2]="out.txt";
+                args[3]=NULL;
             }
-            fprintf(stderr,"child: %s %s %s\n", args[0],args[1], args[2]);
             close(_pipe[0]);
             dup2(_pipe[1], 1);
             execv( args[0], args );
@@ -48,15 +47,13 @@ int main(int argc, char* argv[]){
         }else{
             close(_pipe[1]);
             wait( &status );
-            printf("parent: child status=%d\n",status);
             FILE *file=fopen("out.txt","w+");
-            sprintf(str,"%s","\0");
+            memset(str,0,strlen(str));
             read(_pipe[0],&str, sizeof(str));
-            printf("parent: %s %s\n", tokens[i], str);
             fprintf(file,"%s",str);
             fclose(file);
         }
     }
-    printf("A%sA\n", str);
+    printf("%s\n", str);
     exit(EXIT_SUCCESS);
 }
