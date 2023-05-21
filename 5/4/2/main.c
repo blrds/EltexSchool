326cce2cb520a20fcb7f1e11f7ad9c4cb95d776e
blrds <blad.mob@mail.ru>
2023-05-21 15:56:21 +0700

    5.4.2

---
+++ 5/4/2/main.c
@@ -0,0 +1,7 @@
+#include "operations.c"
+#include <stdio.h>
+
+void main(){
+    printf("5+7=%lf\n", sum(5,7));
+    printf("14.2-5.2=%lf\n", dif(14.2,5.2));
+}
---
+++ 5/4/2/operations.c
@@ -0,0 +1,7 @@
+double sum(double a, double b){
+    return a+b;
+}
+
+double dif(double a, double b){
+    return a-b;
+}
